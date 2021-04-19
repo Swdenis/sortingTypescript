34d0741b8dbacafaf74f1733981a35b673d3c61c
Swdenis <swdenis@mail.ru>
2021-04-19 17:48:40 +0200

    Added linked list sorting

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
+import { CharactersCollection } from "./CharactersCollection"
+import { LinkedList } from "./LinkedList"
 import { NumbersCollection } from "./NumbersCollection"
 import { Sorter } from "./Sorter"
 const numbersCollection = new NumbersCollection([10000,3,-5,0])
+const charactersCollection = new CharactersCollection('XssadasbBJOIk')
 const sorter = new Sorter(numbersCollection)
+const sorterString = new Sorter(charactersCollection)
+const linkedList = new LinkedList()
+
+linkedList.add(1)
+linkedList.add(-231)
+linkedList.add(0)
+linkedList.add(1500)
+linkedList.add(-2)
+
 sorter.sort()
-console.log(sorter.collection)
+console.log(numbersCollection.data)
+
+sorterString.sort()
+console.log(charactersCollection.data)
+
+
+const sorterList = new Sorter(linkedList)
+sorterList.sort()
+linkedList.print()
